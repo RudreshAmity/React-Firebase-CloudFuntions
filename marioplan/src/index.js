a81fec2c86883ccf95f94fcf719aeff385318258
RudreshAmity <rudresh.narwal@gmail.com>
2018-12-06 00:17:16 +0530

    Thunk Update

--- marioplan/src/index.js
+++ marioplan/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux'; /* import create store from redux library */
+import { createStore, applyMiddleware } from 'redux'; /* import create store from redux library & appling middleware function*/
 import rootReducer from './store/reducers/rootReducer';
 /* importing provider component which can surround our app and pass the store into the 
 application so that the application has access to the store */
-import { Provider } from 'react-redux' // react-redux is the binding layer. This help us to bind redux with our react app
+import { Provider } from 'react-redux'; // react-redux is the binding layer. This help us to bind redux with our react app
+import thunk from 'redux-thunk'; // importing redux thunk and applying middleware inside the store
 
-
-const store = createStore(rootReducer); // creating store
+const store = createStore(rootReducer, applyMiddleware(thunk)); // creating store & applying middleware which enhance our store with extra functionality
 //passing the store into the Provider
 ReactDOM.render(<Provider store = {store }><App /></Provider>, document.getElementById('root'));
 
--- marioplan/src/store/reducers/projectReducer.js
+++ marioplan/src/store/reducers/projectReducer.js
@@ -7,6 +7,13 @@ const initState = {
 }
 
 const projectReducer = ( state = initState, action ) => {
-        return state
+    switch(action.type){
+        case 'CREATE_PROJECT':
+               console.log('created project',action.project);
+               break;
+        default: console.log('problem in projectReducer');
+        
+    }   
+    return state
 }
 export default projectReducer;
