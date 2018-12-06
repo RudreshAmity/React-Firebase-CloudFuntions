d04cbb2c26b199851fbc41476cd94f9443e17ea1
RudreshAmity <rudresh.narwal@gmail.com>
2018-12-07 00:11:50 +0530

    adding data to firbase

--- marioplan/src/store/reducers/projectReducer.js
+++ marioplan/src/store/reducers/projectReducer.js
@@ -10,10 +10,12 @@ const projectReducer = ( state = initState, action ) => {
     switch(action.type){
         case 'CREATE_PROJECT':
                console.log('created project',action.project);
-               break;
-        default: console.log('problem in projectReducer');
-        
+               return state;
+        case 'CREATE_PROJECT_ERROR':
+               console.log('created project error',action.err);
+               return state;
+        default: 
+               return state;
     }   
-    return state
 }
 export default projectReducer;
