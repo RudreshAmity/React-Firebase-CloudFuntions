4e764eae847085c2c3a4f4822fa09947ca14f036
RudreshAmity <rudresh.narwal@gmail.com>
2018-12-06 23:35:02 +0530

    react-redux-firebase-forestore update

--- marioplan/src/index.js
+++ marioplan/src/index.js
@@ -3,14 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux'; /* import create store from redux library & appling middleware function*/
+import { createStore, applyMiddleware, compose } from 'redux'; /* import create store from redux library & appling middleware function && compose is used to combine several store enhancer*/
 import rootReducer from './store/reducers/rootReducer';
 /* importing provider component which can surround our app and pass the store into the 
 application so that the application has access to the store */
 import { Provider } from 'react-redux'; // react-redux is the binding layer. This help us to bind redux with our react app
 import thunk from 'redux-thunk'; // importing redux thunk and applying middleware inside the store
+import { reduxFirestore, getFirestore } from 'redux-firestore'
+import { recatReduxFirebase, getFirebase, reduxFirebase } from 'react-redux-firebase'
+import fbConfig from './config/fbConfig'
 
-const store = createStore(rootReducer, applyMiddleware(thunk)); // creating store & applying middleware which enhance our store with extra functionality
+const store = createStore(rootReducer,
+    compose( 
+      applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
+      reduxFirestore(fbConfig),
+      reduxFirebase(fbConfig)      
+     )
+); // creating store & applying middleware which enhance our store with extra functionality
 //passing the store into the Provider
 ReactDOM.render(<Provider store = {store }><App /></Provider>, document.getElementById('root'));
 
