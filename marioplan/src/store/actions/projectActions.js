2b692de5370ce98d8cb0287de6a9b2f98359c7ae
RudreshAmity <rudresh.narwal@gmail.com>
2018-12-07 00:15:10 +0530

    update

--- marioplan/src/store/actions/projectActions.js
+++ marioplan/src/store/actions/projectActions.js
@@ -1,13 +1,14 @@
 export const createProject = (project) => {
     return(dispatch, getState, { getFirebase, getFirestore }) => {
            //make async call to database
+           //pausing the dispatch
            const firestore = getFirestore;  //this give us reference to firestore database
            firestore.collection('projects').add({ //passing object {} into this add method
                 ...project,
                 authorFirstName : 'Rudresh',
                 authorLastName : 'Narwal',
                 authorId : 12345,
-                createdAt : new Date()
+                createdAt : new Date()  
            }).then(() => {
                 dispatch({ type : 'CREATE_PROJECT', project }); //disptaching action to reducer 
            }).catch((err) => {
