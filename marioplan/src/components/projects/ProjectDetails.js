28b55c4399f740fe6d1243c2eae6ae9824a46347
RudreshAmity <rudresh.narwal@gmail.com>
2018-12-08 00:49:31 +0530

    project id updates

--- marioplan/src/components/projects/ProjectDetails.js
+++ marioplan/src/components/projects/ProjectDetails.js
@@ -1,23 +1,51 @@
 import React from 'react';
+import { connect } from 'react-redux'
+import { firestoreConnect } from 'react-redux-firebase' // we want to use this thing as HOC at the bottom to connect this component with firstore collection.
+import { compose } from 'redux'
 
 const ProjectDetails = (props) => {
     
-    const id = props.match.params.id;
+    //console.log(props);
+    const { project } = props; 
+    if(project){
+      return(
+      <div className="container section project-details">
+        <div className="card z-depth-0">
+          <div className="card-content">
+              <span className="card-title">{ project.title }</span>
+              <p>{ project.content }</p>
+          </div>
+          <div className="card-action grey lighten-4 grey-text">
+              <div>Posted by { project.authorFirstName } { project.authorLastName } </div>
+              <div>2nd Sept, 2am</div>
+          </div>
+        </div>       
+      </div>
+     ) 
+    }
+    else{
+        return(
+              <div className="container center">
+                  <p>Loading Project...</p>
+              </div>
+            )
+    }
 
-    return(
-        <div className="container section project-details">
-          <div className="card z-depth-0">
-            <div className="card-content">
-                <span className="card-title">Project Title-{id}</span>
-                <p>Lorem Ipsum dolor ballh bajhgdj shds sj dgjsagj s sjds this ti s another levelof it.</p>
-            </div>
-            <div className="card-action grey lighten-4 grey-text">
-                <div>Posted by The Net Ninja</div>
-                <div>2nd Sept, 2am</div>
-            </div>
-          </div>       
-        </div>
-    )
+   
+}
+const mapStateToProps = (state, ownProps) => {  //ownProps is props of the component 
+    //console.log(state);
+    const id = ownProps.match.params.id;   // ownProps is used to use props in this mapState to props, as we can't access props in this fuction directly 
+    const projects = state.firestore.data.projects; // see on console using console.log(state)
+    const project = projects ? projects[id] : null // projects is the object that we see in our and if there is any project then we find the project on the projects object with that id  
+    return{
+       project: project
+    }
 }
 
-export default ProjectDetails;
+export default compose(
+    connect(mapStateToProps),
+    firestoreConnect([  // firestore connect to single collection
+        { collection: 'projects' }
+])
+)(ProjectDetails);
--- marioplan/src/components/projects/ProjectList.js
+++ marioplan/src/components/projects/ProjectList.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ProjectSummary from './ProjectSummary'
+import { Link } from 'react-router-dom'
 //receiving props from dashboard.js
 const ProjectList = ({projects}) => {
     return(
         <div className="project-list section">
            { projects && projects.map(project => { // here if we have projects then it will execute projects.map
                return( //passing each project as props to Project summary
+               <Link to={'/project/'+project.id}>
                    <ProjectSummary project={project} key={project.id}/>
+               </Link>    
                )
            })}        
         </div>
